4158f7bc32d8f30842f051e3c14b62f4e24958ec
lexdolfing <lexdolfing@gmail.com>
2023-03-29 13:27:19 +0200

    added blogpost content and overview content, finished assignment 1

--- src/pages/blogpost/Blogpost.js
+++ src/pages/blogpost/Blogpost.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import posts from '../../data/posts.json'
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 export default function Blogpost() {
-console.log(posts)
+    const {blogId} = useParams();
+
+    const currentPost = posts.find((post) => {
+        return post.id === blogId;
+    })
 
     return(
-        <></>
+        <>
+        <h1>{currentPost.title}</h1>
+            <p>{currentPost.content}</p>
+            <p>This post was added on {currentPost.date}</p>
+        </>
     )
 }
--- src/pages/blogpostsOverview/BlogpostsOverview.js
+++ src/pages/blogpostsOverview/BlogpostsOverview.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import blogPosts from '../../data/posts.json'
+import { Outlet, Link} from 'react-router-dom'
 
 
 export default function BlogpostsOverview() {
     return (
         <>
             <h1>Overview of blogposts</h1>
-            <p>Aantal blogposts: X</p>
+            <p>Aantal blogposts: {blogPosts.length}</p>
+            <ul>
+                {blogPosts.map((post) => {
+                    return <li key={post.id}>
+                        <Link to={'/blogposts/{post.id}'}>{post.title}</Link>
+                    </li>
+                } )}
+            </ul>
         </>
     )
 }
+
